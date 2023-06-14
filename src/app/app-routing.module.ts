e94a0e5e29e38b2eb542b2e268a6aef4d78afa0e
Camilo Mazorra <mazorra006@gmail.com>
2023-06-14 11:16:32 -0500

    router outlet components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorComponent } from './screens/home/author/author/author.component';
+import { BannerComponent } from './screens/home/banner/banner/banner.component';
+import { NewsComponent } from './screens/home/news/news/news.component';
+import { RecomendationsComponent } from './screens/home/recomendations/recomendations/recomendations.component';
+import { LoginComponent } from './screens/login/login/login.component';
+import { CartComponent } from './screens/cart/cart/cart.component';
+import { NavbarComponent } from './shared/components/navbar/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AuthorComponent },
+  { path: 'author', component: AuthorComponent },
+  { path: 'banner', component: BannerComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'recomendations', component: RecomendationsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'footer', component: FooterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
